29caed8dcb76814ae6e41eede370a33a7ed924d2
marklovejoydev <marklovejoydev@gmail.com>
2024-08-22 09:25:34 +0100

    update to usePopcorn implemented Api into fetch request

--- my_pojects/usepopcorn/src/index.js
+++ my_pojects/usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
@@ -17,20 +17,6 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={5} color="red" size={24} className="test" />
-    <StarRating
-      maxRating={5}
-      color="red"
-      size={24}
-      className="test"
-      defaultRating={3}
-    /> */}
-
-    <Test />
+    <App />
   </React.StrictMode>
 );
